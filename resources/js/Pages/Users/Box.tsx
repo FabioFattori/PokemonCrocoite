8241d142dc104a83f82d8538a36b10bea1448895
FabioFattori <fabiofattoriw@gmail.com>
2024-07-18 14:30:00 +0200

    bug fix

--- resources/js/Pages/Users/Box.tsx
+++ resources/js/Pages/Users/Box.tsx
@@ -4,6 +4,7 @@ import { Button, setTableToUse } from "../../utils/buttons";
 import SideBar from "../../Components/SideBar";
 import userMode from "../../Components/userMode";
 import OutboxIcon from '@mui/icons-material/Outbox';
+import React from "react";
 
 function Box() {
     var boxes = (usePage().props.boxes as any[]) ?? null;
@@ -11,9 +12,14 @@ function Box() {
 
     let btn = [{
         label: "Mostra Pokemon all'interno", icon: OutboxIcon, method:({ props }: { props: any }) => {
-            router.get("/user/exemplariesInBox", { id: props[0].id });
+            router.get("/user/exemplariesInBox", { box_id: props[0].id });
         }
     }] as unknown as Button[];
+
+    React.useEffect(() => {
+        console.log(window.location.href);
+    },[]);
+
     return (
         <>
             <SideBar title={"Tutti i tuoi Box"} mode={userMode.user}/>
