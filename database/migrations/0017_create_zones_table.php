744ec1bb35f441a1f98d9fbca895446e57f34471
mtttia <mattia.senni.03@gmail.com>
2024-07-20 11:49:22 +0200

    fix

--- database/migrations/0017_create_zones_table.php
+++ database/migrations/0017_create_zones_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('zones', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->integer("length");
             $table->integer("width");
             $table->unique("name");
